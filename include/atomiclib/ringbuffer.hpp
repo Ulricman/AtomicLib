315e261d5857d63d275d65a53ff56a7f8e961ee0
Jeffrey <ulricmen@gmail.com>
2025-02-23 00:29:52 +0800

    WIP: move development platform from mac to linux

--- include/atomiclib/ringbuffer.hpp
+++ include/atomiclib/ringbuffer.hpp
@@ -3,63 +3,66 @@
 
 #include <atomic>
 #include <new>
+#include <string>
 #include <utility>
 
-namespace lockfreelib {
+namespace atomiclib {
 
 template <typename T>
 class RingBuffer {
  private:
   const uint64_t capacity_;
   T* data_;
-  std::atomic<uint64_t> head_, tail_, size_;
+  uint8_t *push_mask_, *pop_mask_;
+  std::atomic<uint64_t> head_, tail_;
 
  public:
-  RingBuffer(uint64_t _capacity_)
-      : capacity_(_capacity_), head_(0), tail_(0), size_(0) {
+  RingBuffer(uint64_t _capacity_) : capacity_(_capacity_), head_(0), tail_(0) {
     data_ = static_cast<T*>(::operator new(sizeof(T) * capacity_));
+
+    push_mask_ =
+        static_cast<uint8_t*>(::operator new(sizeof(uint8_t) * capacity_));
+    memset(push_mask_, 0, sizeof(uint8_t) * capacity_);
+
+    pop_mask_ =
+        static_cast<uint8_t*>(::operator new(sizeof(uint8_t) * capacity_));
+    memset(pop_mask_, 0, sizeof(uint8_t) * capacity_);
   }
 
   ~RingBuffer() { ::operator delete(data_); }
 
-  uint64_t size() const { return size_.load(std::memory_order_relaxed); }
-
-  bool push(T&& entry) {
-    if (size_.load(std::memory_order_relaxed) == capacity_) {
-      return false;
-    }
-    uint64_t tail = tail_.load(std::memory_order_relaxed);
-    while (tail_.compare_exchange_weak(tail, (tail + 1) % capacity_));
-    new (&data_[tail_]) T(std::forward<T>(entry));
-    size_.fetch_add(1, std::memory_order_release);
-    return true;
+  void push(T& entry) {
+    uint64_t idx = tail_.load(std::memory_order_acquire);
+    while ((idx + 1) % capacity_ == head_.load(std::memory_order_acquire) ||
+           pop_mask_[(idx + 1) % capacity_] == 1 ||
+           !tail_.compare_exchange_weak(idx, (idx + 1) % capacity_,
+                                        std::memory_order_release,
+                                        std::memory_order_relaxed));
+    new (&data_[idx]) T(entry);
+    push_mask_[idx] = 1;
   }
 
-  template <typename... Args>
-  bool emplace(Args&&... args) {
-    if (size_.load(std::memory_order_relaxed) == capacity_) {
-      return false;
-    }
-    uint64_t tail = tail_.load(std::memory_order_relaxed);
-    while (tail_.compare_exchange_weak(tail, (tail + 1) % capacity_));
-    new (&data_[tail]) T(std::forward<Args>(args)...);
-    size_.fetch_add(1, std::memory_order_release);
-    return true;
-  }
+  // template <typename... Args>
+  // void emplace(Args&&... args) {
+  //   uint64_t tail = tail_.load(std::memory_order_relaxed);
+  //   while ((tail + 1) % capacity_ == head_.load(std::memory_order_relaxed) ||
+  //          !tail_.compare_exchange_weak(tail, (tail + 1) % capacity_));
+  //   new (&data_[tail]) T(std::forward<Args>(args)...);
+  // }
 
-  bool pop(T& entry) {
-    if (size_.load(std::memory_order_relaxed) == 0) {
-      return false;
-    }
-    uint64_t head = head_.load(std::memory_order_relaxed);
-    while (head_.compare_exchange_weak(head, (head + 1) % capacity_));
-    entry = data_[head];
-    data_[head].~T();
-    size_.fetch_sub(1, std::memory_order_release);
-    return true;
+  void pop(T& entry) {
+    uint64_t idx = head_.load(std::memory_order_acquire);
+    while (idx == tail_.load(std::memory_order_acquire) ||
+           push_mask_[idx] == 0 ||
+           !head_.compare_exchange_weak(idx, (idx + 1) % capacity_,
+                                        std::memory_order_relaxed,
+                                        std::memory_order_relaxed));
+    entry = data_[idx];
+    data_[idx].~T();
+    pop_mask_[idx] = 0;
   }
 };  // RingBuffer
 
-}  // namespace lockfreelib
+}  // namespace atomiclib
 
 #endif
